bd62120a32710ff2b166a53778a8187c40cfe5fd
yalcinyakar <yalcinyakar@icloud.com>
2023-02-20 11:59:32 +0300

    akordeon menü saçmalaması düzeldi, footer scriptlerindeki pathler düzeltildi ama daha da mı bozuldu bilemiyoruz, bakıcaz.

--- scripts/render-scripts.js
+++ scripts/render-scripts.js
@@ -7,7 +7,7 @@ const sh = require('shelljs');
 module.exports = function renderScripts() {
 
     const sourcePath = upath.resolve(upath.dirname(__filename), '../src/js');
-    const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
+    const destPath = upath.resolve(upath.dirname(__filename), '../public/.');
 
     sh.cp('-R', sourcePath, destPath)
 
