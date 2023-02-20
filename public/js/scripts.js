5ec3decc37f4d48b6c4666502315f4d7e85cd56a
yalcinyakar <yalcinyakar@icloud.com>
2023-02-20 12:33:12 +0300

    fix for nothing

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,2 @@
-/*!
-* ImposterSoft - Modern Business v5.0.6 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2022 ImposterSoft
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
